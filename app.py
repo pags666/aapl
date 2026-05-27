397f2bf8541326f39e77e0d775ca1920f2353af2
pags666 <pags40502@gmail.com>
2026-05-27 20:56:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ fig.update_traces(
 
 fig.update_layout(
 
-    title="AAPALLA Central Hospital Monitoring System",
+    title="AAPAL Central Hospital Monitoring System",
 
     margin=dict(
         t=50,
@@ -215,7 +215,7 @@ fig.update_layout(
 # SHOW TREEMAP
 # =========================================================
 
-st.title("AAPALLA Central Hospital Monitoring System")
+st.title("AAPAL Central Hospital Monitoring System")
 
 st.plotly_chart(
     fig,
